8200c5819ce5430ce818ebe5bfc8ef571c319126
Nghia Nguyen <projects.futuresky@gmail.com>
2020-07-16 16:36:03 +0700

    Update lasted edit

---
+++ app/src/main/java/vn/vistark/pharmass/ui/pharmacy_bill/PharmacyBillActivity.kt
@@ -0,0 +1,12 @@
+package vn.vistark.pharmass.ui.pharmacy_bill
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import vn.vistark.pharmass.R
+
+class PharmacyBillActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pharmacy_bill)
+    }
+}
